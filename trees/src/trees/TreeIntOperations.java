ebbc8a34558c37d18a6a28afaaed5215092fd800
Rakshith <rakshithmamidala67@gmail.com>
2019-08-21 13:51:53 +0530

    Modifications.

--- trees/src/trees/TreeIntOperations.java
+++ trees/src/trees/TreeIntOperations.java
@@ -1,7 +1,20 @@
 package trees;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 
+ * @author Rakshith
+ */
 public class TreeIntOperations {
 
+	/**
+	 * determines whether the tree is balanced or not. A Tree is balanced if
+	 * difference in heights of let and right is less than or equal to 1
+	 * 
+	 * @param root - the root of the tree
+	 */
 	public void checkIfBalanced(Node root) {
 		if (balancedTree(root)) {
 			System.out.println("Balanced Tree");
@@ -26,16 +39,11 @@ public class TreeIntOperations {
 		}
 	}
 
-	public int sizeOfTree(Node root) {
-		if (root == null) {
-			return 0;
-		} else {
-			int leftSize = 1 + sizeOfTree(root.getLeft());
-			int rightSize = 1 + sizeOfTree(root.getRight());
-			return leftSize + rightSize - 1;
-		}
-	}
-
+	/**
+	 * determines the depth or height of the tree.
+	 * 
+	 * @param root - the root of the tree
+	 */
 	public int height(Node root) {
 		if (root == null) {
 			return 0;
@@ -47,4 +55,117 @@ public class TreeIntOperations {
 		int height = leftHeight > rightHeight ? leftHeight : rightHeight;
 		return height;
 	}
+
+	/**
+	 * determines the size or number of nodes of the tree.
+	 * 
+	 * @param root - the root of the tree
+	 */
+	public int sizeOfTree(Node root) {
+		if (root == null) {
+			return 0;
+		} else {
+			return sizeOfTree(root.getRight()) + sizeOfTree(root.getLeft()) + 1;
+		}
+	}
+
+	/**
+	 * determines the number of leaves of the tree.
+	 * 
+	 * @param root - the root of the tree
+	 */
+	public int numberOfLeaves(Node root) {
+		if (root == null) {
+			return 0;
+		} else if (root.getLeft() == null && root.getRight() == null) {
+			return 1;
+		} else {
+			return numberOfLeaves(root.getLeft()) + numberOfLeaves(root.getRight());
+		}
+	}
+
+	/**
+	 * determines the sum of values of all nodes of the tree.
+	 * 
+	 * @param root - the root of the tree
+	 */
+	public int sumOfTree(Node root) {
+		if (root == null) {
+			return 0;
+		} else if (root.getLeft() == null && root.getRight() == null) {
+			return root.getValue();
+		} else {
+			return root.getValue() + sumOfTree(root.getLeft()) + sumOfTree(root.getRight());
+		}
+	}
+
+	/**
+	 * determines the sum of values of all leaf nodes of the tree.
+	 * 
+	 * @param root - the root of the tree
+	 */
+	public int sumOfLeaves(Node root) {
+		if (root == null) {
+			return 0;
+		} else if (root.getLeft() == null && root.getRight() == null) {
+			return root.getValue();
+		} else {
+			return sumOfLeaves(root.getLeft()) + sumOfLeaves(root.getRight());
+		}
+	}
+
+	/**
+	 * determines the maximum element in the tree.
+	 * 
+	 * @param root - the root of the tree
+	 */
+	public int maxElement(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		Deque<Node> deque = new ArrayDeque<>();
+		deque.add(root);
+		int maximum = root.getValue();
+		while (!deque.isEmpty()) {
+			Node currentNode = deque.removeFirst();
+			if (currentNode.getValue() > maximum) {
+				maximum = currentNode.getValue();
+			}
+			if (currentNode.getRight() != null) {
+				deque.add(currentNode.getRight());
+			}
+			if (currentNode.getLeft() != null) {
+				deque.add(currentNode.getLeft());
+			}
+		}
+		return maximum;
+	}
+
+	/**
+	 * determines the maximum element in the tree.
+	 * 
+	 * @param root - the root of the tree
+	 */
+	public int minElement(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		Deque<Node> deque = new ArrayDeque<>();
+		deque.add(root);
+		int minimum = root.getValue();
+		while (!deque.isEmpty()) {
+			Node currentNode = deque.removeFirst();
+			if (currentNode.getValue() < minimum) {
+				minimum = currentNode.getValue();
+			}
+			if (currentNode.getRight() != null) {
+				deque.add(currentNode.getRight());
+			}
+			if (currentNode.getLeft() != null) {
+				deque.add(currentNode.getLeft());
+			}
+		}
+		return minimum;
+	}
+
 }
